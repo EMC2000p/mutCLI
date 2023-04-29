36d0e35a2af45dc05a171ad811e179c82cd5f1d4
EMC2000p <103334659+EMC2000p@users.noreply.github.com>
2023-04-29 14:11:19 -0400

    Update mutCLI.bat

--- mutCLI.bat
+++ mutCLI.bat
@@ -1,5 +1,5 @@
 @echo off
-title mutCLI (b5)
+title mutCLI (b6)
 echo Welcome to mutCLI!
 echo Type "help" for help!
 :a
@@ -7,17 +7,20 @@ set /P a="mutCLI>"
 if /I "%a%" EQU "help" goto b
 if /I "%a%" EQU "exit" exit
 if /I "%a%" EQU "tools" goto d
+if /I "%a%" EQU "notes" goto c
 echo Command not found or you typed nothing
 goto a
 
 :b
+echo Type "notes" to see patch notes
 echo Type "tools" for system tools
 echo Type "exit" to exit
 echo Expect more from the later releases
 goto a
 
 :c
-
+more notes.txt
+goto a
 
 :d
 scripts\tools.bat
